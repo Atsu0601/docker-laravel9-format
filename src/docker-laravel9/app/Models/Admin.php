534d74719c598a14fb683be784e15da18d2d04e5
Kawatani Atsushi <katsushi61@gmail.com>
2023-03-29 23:00:41 +0900

    管理者ログインの実装

--- src/docker-laravel9/app/Models/Admin.php
+++ src/docker-laravel9/app/Models/Admin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User; //追加
 use Illuminate\Notifications\Notifiable; //追加
+use App\Notifications\ResetPasswordNotification;            //追記
 
 class Admin extends User
 {
@@ -25,4 +26,10 @@ class Admin extends User
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)       //追記
+    {                                                           //追記
+        $url = url("admin/password/reset/$token");              //追記
+        $this->notify(new ResetPasswordNotification($url));     //追記
+    }
 }
